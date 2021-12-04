1d6e79cb4883dd31ef9565a66da5903b25ea2774
Melody <2042204285@qq.com>
2021-12-04 11:54:55 +0000

    更新了首次打开报错问题

--- src/components/top-ranking/index.js
+++ src/components/top-ranking/index.js
@@ -1,5 +1,4 @@
 import React, { memo } from "react";
-// import { useDispatch } from "react-redux";
 
 import { getSizeImage } from "@/utils/format-utils";
 import {
@@ -13,7 +12,11 @@ import { useDispatch } from "react-redux";
 export default memo(function HYTopRanking(props) {
   // props and state
   const { info } = props;
-  const { tracks = [] } = info;
+
+  // const tracks = info && info.tracks;
+  const tracks = info?.tracks;
+  const cover = info?.coverImgUrl;
+  const name = info?.name;
 
   // redux hooks
   const dispatch = useDispatch();
@@ -31,13 +34,13 @@ export default memo(function HYTopRanking(props) {
     <TopRankingWrapper>
       <div className="header">
         <div className="image">
-          <img src={getSizeImage(info.coverImgUrl)} alt="" />
+          <img src={getSizeImage(cover)} alt="" />
           <a href="/" className="image_cover">
             ranking
           </a>
         </div>
         <div className="info">
-          <a href="/">{info.name}</a>
+          <a href="/">{name}</a>
           <div>
             <button className="btn play sprite_02"></button>
             <button className="btn favor sprite_02"></button>
@@ -45,24 +48,25 @@ export default memo(function HYTopRanking(props) {
         </div>
       </div>
       <div className="list">
-        {tracks.slice(0, 10).map((item, index) => {
-          return (
-            <div key={item.id} className="list-item">
-              <div className="rank">{index + 1}</div>
-              <div className="info">
-                <span className="name text-nowrap">{item.name}</span>
-                <div className="operate">
-                  <button
-                    className="btn sprite_02 play"
-                    onClick={(e) => playMusic(e, item)}
-                  ></button>
-                  <button className="btn sprite_icon2 addto"></button>
-                  <button className="btn sprite_02 favor"></button>
+        {tracks &&
+          tracks.slice(0, 10).map((item, index) => {
+            return (
+              <div key={item.id} className="list-item">
+                <div className="rank">{index + 1}</div>
+                <div className="info">
+                  <span className="name text-nowrap">{item.name}</span>
+                  <div className="operate">
+                    <button
+                      className="btn sprite_02 play"
+                      onClick={(e) => playMusic(e, item)}
+                    ></button>
+                    <button className="btn sprite_icon2 addto"></button>
+                    <button className="btn sprite_02 favor"></button>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
       <div className="footer">
         <a href="/">查看全部 &gt;</a>
